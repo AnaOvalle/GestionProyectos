2fe8be622ea023a06e84a9e3e9685ae94edde6f5
Ana Ovalle <l21212014@tectijuana.edu.mx>
2024-10-20 17:12:55 -0700

    20/10/24 Ana

---
+++ ProyectoBiblioteca/Colecciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBiblioteca
+{
+    public partial class Colecciones : Form
+    {
+        public Colecciones()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
